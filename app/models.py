c859fa5999b01984f87a077e588b84156aa320a4
ceenen <phunh.work@gmail.com>
2025-06-16 04:44:13 +0700

    [Fix] Fix email sender
    There are still errors in Auth, like JWT manager init

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    user_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(unique=True, nullable=False)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+import os
+
+MODEL_DIR = os.path.join(os.path.dirname(__file__), 'app', 'llm_models')
+MODEL_LIST = [
+    "gemma-3-1b-it-Q8_0.gguf",
+    "Llama-3.2-1B-Instruct-GRPO-GGUF.gguf",
+    "Llama-3.2-1B-Instruct-Q8_0.gguf"
+]
+
 if __name__ == "__main__":
     import app
     pp = app.create_app_with_blueprint()
